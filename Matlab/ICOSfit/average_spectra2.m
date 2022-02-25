e8684b2631edbbefa7bd8a60ee79ba41929e4bb7
Norton Allen <allen@huarp.harvard.edu>
2022-02-25 15:58:42 -0500

    Average P and T (duh)

--- Matlab/ICOSfit/average_spectra2.m
+++ Matlab/ICOSfit/average_spectra2.m
@@ -46,10 +46,12 @@ while T0 <= T(end)
     mlf_mkdir(obase, new_scannum);
     writebin(mlf_path(obase,new_scannum),[icos,etln]);
     nearest_scan_index = interp1(binscans, find(V), new_scannum,'nearest');
-    PTEnew = PTE(nearest_scan_index,2:12);
+    Pavg = mean(PTE(V,2));
+    Tavg = mean(PTE(V,3));
+    PTEnew = PTE(nearest_scan_index,4:12);
     fprintf(PTEo, ...
         '%d %.2f %.1f %d %.7g %.7g %.7g %.7g %.7g %.7g %.7g %.6g %d\n', ...
-        new_scannum, PTEnew, n_avg);
+        new_scannum, Pavg, Tavg, PTEnew, n_avg);
   end
   T0 = T0+secs_avg;
 end
