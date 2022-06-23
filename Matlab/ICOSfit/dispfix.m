d805cee34601d9e35e4effdc915c0b1d2174d1b5
Norton Allen <allen@huarp.harvard.edu>
2022-06-22 21:06:21 -0500

    dispfix.m: remove ICOSsetup dependence. Delete basefilefromresid.m

--- Matlab/ICOSfit/dispfix.m
+++ Matlab/ICOSfit/dispfix.m
@@ -1,4 +1,4 @@
-function fdata = dispfix( base );
+function fdata = dispfix( base )
 % fitdata = dispfix( base, by_molecule );
 % is by_molecule relevant?
 % I'll simply try to display each fit parameter as a single line of points
@@ -7,28 +7,27 @@ function fdata = dispfix( base );
 % If set to 2, will be partitioned by isotopomer
 % If set to 0, all parameters will be shown on one plot
 % If base is omitted, will default to 'ICOSout'
-by_molecule=0;
-ICOSsetup;
+S = ICOS_setup(base);
 run = getrun;
 
 % v = [0:nlines-1]*n_line_params + inputparams+ n_base_params + 1;
 % vf0 = inputparams+2*n_base_params+n_lines*n_line_params;
 % select = [ 0 1 2 3 ];
-n_line_params = n_line_params + n_abs_line_params;
-n_base_params = n_base_params + n_abs_params;
-vf = [1:n_lines*n_line_params] + n_input_params+2*n_base_params+n_lines*n_line_params;
+n_line_params = S.n_line_params + S.n_abs_line_params;
+n_base_params = S.n_base_params + S.n_abs_params;
+vf = (1:S.n_lines*n_line_params) + S.n_input_params+2*n_base_params+S.n_lines*n_line_params;
 % vf = v + n_base_params + n_line_params;
 % scannum = fitdata(:,6);
-nfit = length(scannum);
-bv = fitdata(:,vf);
-bv = [ reshape(bv', n_line_params, nfit*n_lines )' 2*ones(nfit*n_lines,1)];
-bv = reshape(bv', n_lines*(n_line_params+1), nfit);
+nfit = length(S.scannum);
+bv = S.fitdata(:,vf);
+bv = [ reshape(bv', n_line_params, nfit*S.n_lines )' 2*ones(nfit*S.n_lines,1)];
+bv = reshape(bv', S.n_lines*(n_line_params+1), nfit);
 figure;
 colormap([ 0 0 .75; 0 1 1; 0 0 0]);
 nsubplot( 1,4,1,[1 3]);
-h = image(scannum([1 nfit]), [1/(n_line_params+1) n_lines]+.5, bv+1 );
+h = image(S.scannum([1 nfit]), [1/(n_line_params+1) S.n_lines]+.5, bv+1 );
 %set(h,'CDataMapping','Scaled');
-set(gca, 'YTick', [1:n_lines]);
+set(gca, 'YTick', 1:S.n_lines);
 xlabel('Scan Number');
 ylabel('Line Number' );
 title([ 'Free Parameters: ' run '/' base]);
@@ -46,5 +45,5 @@ text( 2, 13, '\gamma_L');
 xlim([ 0 7]-1); ylim([-2 30]);
 set(gca,'Visible','off');
 if nargout >= 1
-  fdata = fitdata;
+  fdata = S.fitdata;
 end
