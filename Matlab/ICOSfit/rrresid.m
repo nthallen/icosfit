b295750f055bf6eacb3a0e5036166782cbc7e96d
Norton Allen <allen@huarp.harvard.edu>
2022-06-23 08:53:59 -0500

    rrresid.m: Eliminate ICOSsetup dependence

--- Matlab/ICOSfit/rrresid.m
+++ Matlab/ICOSfit/rrresid.m
@@ -3,9 +3,7 @@ function [ nu, mresid, nus_out, resids_out ] = rrresid( region, suffix, PTEfile 
 % Return all the residuals for the specified fit.
 base = [ 'ICOSout.' region ];
 if ~isempty(suffix); base = [ base '.' suffix ]; end
-lines=[];
-by_molecule=0;
-ICOSsetup;
+S = ICOS_setup(base);
 xmin = [];
 resids = [];
 nscans = 0;
@@ -18,7 +16,7 @@ for i = scannum'
     xmin = f(1,1);
   else
     if f(1,1) < xmin
-      nc = xmin-f(1,1);
+      % nc = xmin-f(1,1);
       resids = [ NaN*zeros(3,size(resids,2)); resids ];
       xmin = f(1,1);
     end
@@ -29,7 +27,7 @@ for i = scannum'
     resids(f(:,1)-xmin+1,nscans) = f(:,3)-f(:,4);
   end
 end
-if nargin < 3; PTEfile = ''; end
+if nargin < 3; PTEfile = S.PTEFile; end
 x = xmin:xmin+size(resids,1)-1;
 nus = get_nu(region, suffix, x, PTEfile, scannum);
 [ nu, rresid ] = icos_resample(nus,resids,1e-4);
