9a93bb632ad7b19070fc1a413d6f1aeee33b8683
Norton Allen <allen@huarp.harvard.edu>
2022-10-21 11:04:11 -0400

    icosfit_reconfig.m: Add support for changing HITRAN parameters

--- Matlab/ICOSfit/icosfit_reconfig.m
+++ Matlab/ICOSfit/icosfit_reconfig.m
@@ -4,10 +4,32 @@ function icosfit_reconfig(ifile, ofile, varargin)
 % the specified options with the new value.
 % If ofile is empty, ifile is modified in place with a backup stored
 % in ifile.bak.
+oldlines = {};
+newlines = {};
+nlines = 0;
+keywords = {};
+patterns = {};
+pattern_values = {};
+npatterns = 0;
+
+% 1 1  7181.155780  1.505e-20 0.1008   136.7617 0.71 -0.012510 355544
 for i = 1:2:(length(varargin)-1)
-  opts.(varargin{i}) = varargin{i+1};
+  if contains(varargin{i}, ...
+      regexpPattern('\d+ +\d+ +[0-9.]+ +[-0-9.e]+ +[0-9.]+ +[0-9.]+ +[0-9.]+ +[-0-9.]+ +[0-9]+'))
+    nlines = nlines+1;
+    oldlines{nlines} = varargin{i};
+    newlines{nlines} = varargin{i+1};
+  elseif contains(varargin{i},' ')
+    error('Illegal option: "%s"', varargin{i});
+  else
+    npatterns = npatterns+1;
+    keywords{npatterns} = varargin{i};
+    patterns{npatterns} = regexpPattern(['\<' varargin{i} '\>']);
+    pattern_values{npatterns} = varargin{i+1};
+    % opts.(varargin{i}) = varargin{i+1};
+  end
 end
-flds = fields(opts);
+
 ifp = fopen(ifile,'r');
 if ifp < 0; error('Unable to read file "%s"', ifile); end
 if isempty(ofile)
@@ -24,8 +46,14 @@ while true
   end
   hasfld = false;
   if isempty(regexp(tline,'^ *#','once'))
-    for i=1:length(flds)
-      if contains(tline,flds{i})
+    for i=1:length(oldlines)
+      if contains(tline,oldlines{i})
+        tline = replace(tline,oldlines{i},newlines{i});
+        break;
+      end
+    end
+    for i=1:length(patterns)
+      if contains(tline,patterns{i})
         hasfld = true;
         break;
       end
@@ -36,11 +64,11 @@ while true
   end
 end
 fclose(ifp);
-for i=1:length(flds)
-  if isempty(opts.(flds{i}))
+for i=1:npatterns
+  if isempty(pattern_values{i})
     fprintf(ofp, '%s;\n', flds{i});
   else
-    fprintf(ofp, '%s = %s;\n', flds{i}, opts.(flds{i}));
+    fprintf(ofp, '%s = %s;\n', keywords{i}, pattern_values{i});
   end
 end
 fclose(ofp);
